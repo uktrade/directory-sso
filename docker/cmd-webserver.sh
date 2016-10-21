d0bb1fa8cfb10113dd7de2adc1d3096afa2a8295
konradko <konrad.korzel@gmail.com>
2016-10-21 15:35:34 +0100

    Fix swagger view

--- docker/cmd-webserver.sh
+++ docker/cmd-webserver.sh
@@ -3,4 +3,4 @@
 python /usr/src/app/manage.py migrate
 mkdir -p /usr/src/app/sso/static
 python /usr/src/app/manage.py collectstatic --noinput
-gunicorn -c /usr/src/app/gunicorn/conf.py sso.wsgi --bind 0.0.0.0:$PORT --log-file -
+gunicorn -c sso.wsgi --bind 0.0.0.0:$PORT --log-file -
--- sso/views.py
+++ sso/views.py
@@ -1,9 +1,9 @@
-from rest_framework.decorators import sso_view, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 
 
-@sso_view()
+@api_view()
 @renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
 def documentation(request):
     generator = schemas.SchemaGenerator(title='Export Directory API')
