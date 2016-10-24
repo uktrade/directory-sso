5c7c1744dc367fc0ddf9eab86813af7375c0ebf8
konradko <konrad.korzel@gmail.com>
2016-10-24 15:08:27 +0100

    Add tests for user model

--- sso/user/models.py
+++ sso/user/models.py
@@ -1,7 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields['is_staff'] is not True:
+            raise ValueError('Superuser must have is_staff=True')
+
+        if extra_fields['is_superuser'] is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser):
@@ -20,16 +53,18 @@ class User(AbstractBaseUser):
         _('staff status'),
         default=False,
         help_text=_(
-            'Designates whether the user can log into this admin site.'
+            'Designates whether the user can log into admin'
         ),
     )
     is_superuser = models.BooleanField(
         _('superuser status'),
         default=False,
         help_text=_(
-            'Designates whether the user has superuser privileges.'
+            'Designates whether the user has superuser privileges'
         ),
     )
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
