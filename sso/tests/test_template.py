a8cb933dc6bb531cd92a9bc3c8738dc173d997f8
Richard Tier <rikatee@gmail.com>
2016-10-27 17:35:48 +0100

    Password reset style + header and footer changes (#7)
    * Improve style of the password reset page
    
    * Add tests for password reset template and email confirmation template

---
+++ sso/tests/test_template.py
@@ -0,0 +1,24 @@
+from django.template.loader import render_to_string
+
+
+def test_password_reset_email():
+    context = {
+        'password_reset_url': 'http://reset.com',
+    }
+    html = render_to_string(
+        'account/email/password_reset_key_message.html',
+        context
+    )
+    assert 'http://reset.com' in html
+    assert 'Reset password' in html
+
+
+def test_confirmation_email():
+    context = {
+        'activate_url': 'http://confirm.com'
+    }
+    html = render_to_string(
+        'account/email/email_confirmation_message.html', context
+    )
+    assert 'http://confirm.com' in html
+    assert 'Confirm' in html
