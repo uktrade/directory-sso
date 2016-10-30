cdf3baa716523a4cde2190a8230907257c070aae
Monika Sulik <kontakt@monikasulik.net>
2016-10-30 02:46:42 +0000

    Add comment

--- config/helpers.py
+++ config/helpers.py
@@ -9,6 +9,8 @@ def validate_domain(domain):
     if tld_result.domain and tld_result.suffix and not tld_result.subdomain:
         urlparse_result = urlparse(domain)
         if not urlparse_result.scheme:
+            # When no scheme given urlparse doesn't correctly identify
+            # all parts of the url, so adding it temporarily
             urlparse_result = urlparse('http://' + domain)
         if urlparse_result.path or urlparse_result.query:
             return False
