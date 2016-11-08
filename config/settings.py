fe9077428fc8116b5f077c2069537a4bc48fb946
Monika Sulik <kontakt@monikasulik.net>
2016-11-08 14:20:54 +0000

    Clean up

--- config/settings.py
+++ config/settings.py
@@ -236,8 +236,8 @@ ACCOUNT_FORMS = {
 SESSION_COOKIE_DOMAIN = os.environ['SESSION_COOKIE_DOMAIN']
 # env var not same as setting to be more explicit (directory-ui uses same name)
 SESSION_COOKIE_NAME = os.environ['SSO_SESSION_COOKIE']
-SESSION_COOKIE_SECURE = True if (
-    os.getenv('SSO_SESSION_COOKIE_SECURE') == 'true') else False
+SESSION_COOKIE_SECURE = (
+    os.getenv('SSO_SESSION_COOKIE_SECURE', 'true') == 'true')
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
 
