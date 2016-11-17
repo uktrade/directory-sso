2ebfe24c138e82baa1d464e85ac630fceff4ab2e
Richard Tier <rikatee@gmail.com>
2016-11-17 14:20:51 +0000

    Fix external links in emails (#81)

--- sso/templatetags/sso_email.py
+++ sso/templatetags/sso_email.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.urlresolvers import reverse, resolve
 from sso import constants
 
 register = template.Library()
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag
 def header_image():
     return constants.EMAIL_HEADER_IMAGE
+
+
+@register.simple_tag
+def absolute_url(name):
+    url = reverse(name)
+    view = resolve(url)
+    return view.func.view_initkwargs['url']
