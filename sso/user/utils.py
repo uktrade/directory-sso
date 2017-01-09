86ec3534c365382be3540fa91b466616718ea664
Monika Sulik <kontakt@monikasulik.net>
2017-01-09 13:36:41 +0000

    Fix for next param validation
    Penetration tests found a security issue where if the url had "?next=//google.com" appended it would redirect to the url even though it wasn't an allowed domain.

--- sso/user/utils.py
+++ sso/user/utils.py
@@ -18,12 +18,14 @@ def get_url_with_redirect(url, redirect_url):
 
 
 def is_valid_redirect(next_param):
+    extracted_domain = tldextract.extract(next_param)
+
     # Allow internal redirects
-    if next_param.startswith('/'):
+    is_domain = bool(extracted_domain.domain) and bool(extracted_domain.suffix)
+    if next_param.startswith('/') and not is_domain:
         return True
 
     # Otherwise check we allow that domain/suffix
-    extracted_domain = tldextract.extract(next_param)
     domain = '.'.join([extracted_domain.domain, extracted_domain.suffix])
     return (domain in settings.ALLOWED_REDIRECT_DOMAINS) or (
         extracted_domain.suffix in settings.ALLOWED_REDIRECT_DOMAINS)
