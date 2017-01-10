0eb5e72a493dd6373b015a73a5df5d15c162376f
Monika Sulik <kontakt@monikasulik.net>
2017-01-10 11:53:25 +0000

    Load fixtures in docker

--- docker/cmd-integration-test-webserver.sh
+++ docker/cmd-integration-test-webserver.sh
@@ -2,6 +2,7 @@
 
 python /usr/src/app/manage.py migrate
 python /usr/src/app/manage.py create_test_users
+python /usr/src/app/manage.py loaddata test_fixtures/load_tests.json
 mkdir -p /usr/src/app/sso/static
 python /usr/src/app/manage.py collectstatic --noinput
 gunicorn config.wsgi --bind [::1]:$PORT --bind 0.0.0.0:$PORT --log-file -
