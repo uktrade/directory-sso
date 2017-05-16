f93f03f137b463a5f918e3afb0df41191dbe6247
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-05-16 14:33:43 +0100

    added management command for integration test

--- sso/user/management/commands/create_test_users.py
+++ sso/user/management/commands/create_test_users.py
@@ -1,6 +1,9 @@
+import datetime
+
 from allauth.account.models import EmailAddress
 
 from django.core.management.base import BaseCommand
+from oauth2_provider.models import AccessToken, Application
 
 from sso.user.models import User
 
@@ -9,7 +12,10 @@ class Command(BaseCommand):
     help = 'Used to create a users for integration tests.'
 
     def handle(self, *args, **options):
-        create_verified_integration_test_user()
+        verified_user = create_verified_integration_test_user()
+        create_login_for_token_integration_test_user(verified_user)
+        unverified_user = create_unverified_integration_test_user()
+        create_login_for_token_integration_test_user(unverified_user)
 
 
 def create_verified_integration_test_user():
@@ -22,4 +28,38 @@ def create_verified_integration_test_user():
         email='verified@example.com',
         verified=True,
         primary=True,
+    )
+    return user
+
+
+def create_unverified_integration_test_user():
+    user = User.objects.create_user(
+        email='unverified@example.com',
+        password='passwordpassword'
+    )
+    EmailAddress.objects.create(
+        user=user,
+        email='unverified@example.com',
+        verified=False,
+        primary=True,
+    )
+    return user
+
+
+def create_application(user):
+    return Application.objects.create(
+        user=user,
+        client_type=Application.CLIENT_CONFIDENTIAL,
+        authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        name='Test client'
+    )
+
+
+def create_login_for_token_integration_test_user(user):
+    AccessToken.objects.create(
+        user=user,
+        application=create_application(user),
+        token='1234567890',
+        expires=datetime.datetime.now() + datetime.timedelta(days=1000),
+        scope='profile'
     )
