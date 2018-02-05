4d209c1eb302ca68968cba84e65c2a262ae0a3c8
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-05 15:26:30 +0000

    add handy fixtures

--- testapi/conftest.py
+++ testapi/conftest.py
@@ -1,6 +1,9 @@
 from unittest.mock import patch
 
 import pytest
+from rest_framework.test import APIClient
+
+from sso.user import models
 
 
 @pytest.fixture(autouse=True)
@@ -10,3 +13,27 @@ def mock_signature_checker():
     patcher.start()
     yield
     patcher.stop()
+
+
+@pytest.fixture
+def test_api_auth_token():
+    return "debug"
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def user():
+    return models.User.objects.create_user(
+        email='dev@example.com',
+        password='password',
+    )
+
+
+@pytest.fixture
+def authed_client(client, user):
+    client.force_login(user)
+    return client
