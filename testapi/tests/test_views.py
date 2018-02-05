c17b3f67081fc7553fc37dc7cbe25ad14dec67ac
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-05 15:26:42 +0000

    add tests for enabled & disabled testapi [skip ci]

--- testapi/tests/test_views.py
+++ testapi/tests/test_views.py
@@ -1,21 +1,23 @@
 import pytest
 from rest_framework import status
-from rest_framework.test import APIClient
 
 from django.core.urlresolvers import reverse
 
-from config.settings import TEST_API_AUTH_TOKEN
 
-
-@pytest.fixture
-def api_client():
-    return APIClient()
+@pytest.mark.django_db
+def test_get_user_by_email_with_disabled_testapi(authed_client, settings, test_api_auth_token):
+    settings.TEST_API_ENABLE = False
+    response = authed_client.get(
+        reverse("user_by_email", kwargs={"email": "dev@example.com"}),
+        {"token": test_api_auth_token}
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.django_db
-def test_get_user_by_email(api_client):
-    response = api_client.get(
+def test_get_user_by_email_with_enabled_testapi(authed_client, test_api_auth_token):
+    response = authed_client.get(
         reverse("user_by_email", kwargs={"email": "dev@example.com"}),
-        {"token": TEST_API_AUTH_TOKEN}
+        {"token": test_api_auth_token}
     )
     assert response.status_code == status.HTTP_200_OK
