ee54cb46558682df549449b0d4a0763d12148ac5
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-06 11:18:01 +0000

    update tests so that they don't use test api token

--- testapi/tests/test_views.py
+++ testapi/tests/test_views.py
@@ -4,75 +4,59 @@ from rest_framework import status
 
 
 @pytest.mark.django_db
-def test_get_user_by_email_with_enabled_test_api(
-        client, test_api_auth_token, active_user):
+def test_get_user_by_email_with_enabled_test_api(client, active_user):
     response = client.get(
-        reverse("user_by_email", kwargs={"email": active_user.email}),
-        {"token": test_api_auth_token}
+        reverse("user_by_email", kwargs={"email": active_user.email})
     )
     assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.django_db
 def test_should_get_sso_id_and_is_verified_flag_for_active_user(
-        client, test_api_auth_token, active_user):
+        client, active_user):
     response = client.get(
-        reverse("user_by_email", kwargs={"email": active_user.email}),
-        {"token": test_api_auth_token}
-    )
+        reverse("user_by_email", kwargs={"email": active_user.email}) )
     assert response.data == {"is_verified": True, "sso_id": 1}
 
 
 @pytest.mark.django_db
 def test_should_get_sso_id_and_is_verified_flag_for_inactive_user(
-        client, test_api_auth_token, inactive_user):
+        client, inactive_user):
     response = client.get(
-        reverse("user_by_email", kwargs={"email": inactive_user.email}),
-        {"token": test_api_auth_token}
+        reverse("user_by_email", kwargs={"email": inactive_user.email})
     )
     assert response.data == {"is_verified": False, "sso_id": 2}
 
 
 @pytest.mark.django_db
-def test_should_get_404_for_non_existing_user(
-        client, test_api_auth_token):
+def test_should_get_404_for_non_existing_user(client):
     response = client.get(
-        reverse("user_by_email", kwargs={"email": "non_existing@user.com"}),
-        {"token": test_api_auth_token}
+        reverse("user_by_email", kwargs={"email": "non_existing@user.com"})
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.django_db
-def test_should_get_404_when_email_is_not_present(client, test_api_auth_token):
+def test_should_get_404_when_email_is_not_present(client):
     response = client.get(
-        reverse("user_by_email", kwargs={"email": ""}),
-        {"token": test_api_auth_token}
+        reverse("user_by_email", kwargs={"email": ""})
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_should_get_404_when_email_is_none(client):
+    response = client.get(
+        reverse("user_by_email", kwargs={"email": None})
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.django_db
 def test_get_user_by_email_with_disabled_test_api(
-        client, settings, test_api_auth_token, active_user):
+        client, settings, active_user):
     settings.TEST_API_ENABLE = False
     response = client.get(
-        reverse("user_by_email", kwargs={"email": active_user.email}),
-        {"token": test_api_auth_token}
+        reverse("user_by_email", kwargs={"email": active_user.email})
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
-
-
-def test_get_user_by_email_without_auth_token(client):
-    response = client.get(
-        reverse("user_by_email", kwargs={"email": "some@user.com"})
-    )
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-def test_get_user_by_email_with_invalid_auth_token(client):
-    response = client.get(
-        reverse("user_by_email", kwargs={"email": "some@user.com"}),
-        {"token": "invalid token"}
-    )
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
