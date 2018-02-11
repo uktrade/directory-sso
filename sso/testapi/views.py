30062e6bffdf10e8f0907885b1373262752504b1
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-11 17:18:19 +0000

    add delete use by email endpoint to testapi

--- sso/testapi/views.py
+++ sso/testapi/views.py
@@ -1,18 +1,22 @@
 from django.conf import settings
-from django.http import HttpResponseNotFound
-from rest_framework.generics import RetrieveAPIView, get_object_or_404
+from django.http import HttpResponseNotFound, Http404
+from rest_framework.generics import (
+    get_object_or_404,
+    DestroyAPIView,
+    RetrieveAPIView,
+)
 from rest_framework.response import Response
 
 from config.signature import SignatureCheckPermission
 from sso.user import models
 
 
-class UserByEmailAPIView(RetrieveAPIView):
+class UserByEmailAPIView(RetrieveAPIView, DestroyAPIView):
     permission_classes = [SignatureCheckPermission]
     authentication_classes = []
     queryset = models.User.objects.all()
     lookup_field = 'email'
-    http_method_names = ('get', )
+    http_method_names = ('get', 'delete')
 
     def dispatch(self, *args, **kwargs):
         if not settings.FEATURE_TEST_API_ENABLE:
@@ -26,3 +30,11 @@ class UserByEmailAPIView(RetrieveAPIView):
             'is_verified': user.is_active
         }
         return Response(response_data)
+
+    def delete(self, request, email, **kwargs):
+        try:
+            user = get_object_or_404(models.User, email=email)
+            user.delete()
+        except Http404:
+            raise
+        return Response(status=204)
