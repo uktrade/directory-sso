fbc6b2b323508a090a46811bffb3d6a9e1bf4e3f
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-13 15:11:43 +0000

    testapi add option to flag user email as verified

--- sso/testapi/views.py
+++ sso/testapi/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import (
     get_object_or_404,
     DestroyAPIView,
     RetrieveAPIView,
+    UpdateAPIView
 )
 from rest_framework.response import Response
 
@@ -11,12 +12,12 @@ from config.signature import SignatureCheckPermission
 from sso.user import models
 
 
-class UserByEmailAPIView(RetrieveAPIView, DestroyAPIView):
+class UserByEmailAPIView(RetrieveAPIView, DestroyAPIView, UpdateAPIView):
     permission_classes = [SignatureCheckPermission]
     authentication_classes = []
     queryset = models.User.objects.all()
     lookup_field = 'email'
-    http_method_names = ('get', 'delete')
+    http_method_names = ('get', 'delete', 'patch')
 
     def dispatch(self, *args, **kwargs):
         if not settings.FEATURE_TEST_API_ENABLE:
@@ -38,4 +39,17 @@ class UserByEmailAPIView(RetrieveAPIView, DestroyAPIView):
     def delete(self, request, **kwargs):
         email = kwargs['email']
         self.get_user(email).delete()
+        return Response(status=204)
+
+    def patch(self, request, email, **kwargs):
+        user = self.get_object(email=email)
+        is_verified = request.data['is_verified']
+        user.emailaddress_set.update_or_create(
+            email=user.email,
+            defaults={
+                'verified': is_verified,
+                'user_id': user.id,
+                'primary': True
+            }
+        )
         return Response(status=204)
