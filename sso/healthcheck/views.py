c49fc5515003f2847ed31944e348285b9bb7a210
richtier <rikatee@gmail.com>
2018-10-01 08:50:16 +0100

    Add sentry healthcheck

--- sso/healthcheck/views.py
+++ sso/healthcheck/views.py
@@ -1,20 +1,7 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 from directory_healthcheck.views import BaseHealthCheckAPIView
 from health_check.db.backends import DatabaseBackend
 
-from conf.signature import SignatureCheckPermission
 import core.mixins
-
-
-class PingAPIView(core.mixins.NoIndexMixin, APIView):
-
-    permission_classes = (SignatureCheckPermission, )
-    http_method_names = ("get", )
-
-    def get(self, request, *args, **kwargs):
-        return Response(status=200)
 
 
 class DatabaseAPIView(core.mixins.NoIndexMixin, BaseHealthCheckAPIView):
