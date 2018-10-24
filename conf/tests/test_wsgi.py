e4c3fbebfd2ba88f325c394ebb2689ae5f070aed
richtier <rikatee@gmail.com>
2018-10-24 08:06:23 +0100

    Allow proxy to set script name

---
+++ conf/tests/test_wsgi.py
@@ -0,0 +1,31 @@
+from unittest import mock
+
+from bs4 import BeautifulSoup
+import pytest
+
+from conf import wsgi
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('script_name,prefix', (
+    ('/sso', '/sso/accounts/'),
+    ('', '/accounts/')
+))
+def test_set_script_name(rf, script_name, prefix):
+    environ = rf._base_environ(
+        PATH_INFO='/accounts/password/reset/',
+        CONTENT_TYPE="text/html; charset=utf-8",
+        REQUEST_METHOD="GET",
+        HTTP_X_SCRIPT_NAME=script_name,
+    )
+
+    response = wsgi.application(environ=environ, start_response=mock.Mock)
+
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    ids = ['header-register-link', 'header-sign-in-link']
+    for element_id in ids:
+        element = soup.find(id=element_id)
+        assert element.attrs['href'].startswith(prefix)
--- conf/wsgi.py
+++ conf/wsgi.py
@@ -14,7 +14,17 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
+class SetScriptName:
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = environ.get('HTTP_X_SCRIPT_NAME', '')
+        return self.application(environ, start_response)
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application = SetScriptName(application)
