9e767028fd297c373abc6289b52f732fb83611a4
richtier <rikatee@gmail.com>
2018-12-07 11:25:44 +0000

    Remove docker

--- docker/cmd-webserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-python /usr/src/app/manage.py distributed_migrate --noinput
-mkdir -p /usr/src/app/sso/static
-python /usr/src/app/manage.py collectstatic --noinput
-gunicorn conf.wsgi --bind 0.0.0.0:$PORT --log-file -
