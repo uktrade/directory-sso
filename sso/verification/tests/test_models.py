fec8294501b5a2246108a0bd9ab1d26844ec49ab
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-01-21 10:18:47 +0000

    corrected tests

--- sso/verification/tests/test_models.py
+++ sso/verification/tests/test_models.py
@@ -1,21 +1,23 @@
 from datetime import datetime, timedelta
+
 import pytest
+from django.conf import settings
 
 from sso.verification.models import VerificationCode
+
+expiry_days = settings.VERIFICATION_EXPIRY_DAYS
 
 
 @pytest.fixture
 def verification_code():
     return VerificationCode(
-        is_verified=False,
     )
 
 
 @pytest.mark.django_db
 def test_validation_expired(verification_code):
     verification_code.created = datetime.now() - timedelta(
-            days=verification_code.expiry_days+1
-    )
+            days=expiry_days+1)
 
     assert verification_code.is_expired is True
 
@@ -23,6 +25,6 @@ def test_validation_expired(verification_code):
 @pytest.mark.django_db
 def test_verification_not_expired(verification_code):
     verification_code.created = datetime.now() - timedelta(
-        days=verification_code.expiry_days-1
+        days=expiry_days-1
     )
     assert verification_code.is_expired is False
