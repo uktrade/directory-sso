77fdaccc3f361330ce1b71d2e0187da666891ce6
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-03-11 16:47:21 +0000

    remove dublicate validaters

--- conf/settings.py
+++ conf/settings.py
@@ -107,32 +107,6 @@ TEMPLATES = [
     },
 ]
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator')
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.MinimumLengthValidator'
-        ),
-        'OPTIONS': {
-            'min_length': 10,
-        }
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.CommonPasswordValidator',
-        )
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.NumericPasswordValidator',
-        )
-    },
-]
-
 WSGI_APPLICATION = 'conf.wsgi.application'
 
 VCAP_SERVICES = env.json('VCAP_SERVICES', {})
