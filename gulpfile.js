ac113c44c943ab9cbaaaecc5a90a3ca789ac0933
richtier <rikatee@gmail.com>
2019-03-22 09:45:28 +0000

    Upgrade javascript dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch(SASS_FILES, ['sass']);
+  gulp.watch(SASS_FILES, gulp.series('sass'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', gulp.series('sass'));
