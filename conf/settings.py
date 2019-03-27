387fdcba5c71fdd6def03874a3030500a3a858b5
Richard Tier <rikatee@gmail.com>
2019-03-27 12:34:39 +0000

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -485,7 +485,6 @@ FEATURE_FLAGS = {
     ),
     # used by directory-components
     'MAINTENANCE_MODE_ON': env.bool('FEATURE_MAINTENANCE_MODE_ENABLED', False),
-    'EXPORT_JOURNEY_ON': env.bool('FEATURE_EXPORT_JOURNEY_ENABLED', True),
     'NEW_HEADER_FOOTER_ON': env.bool(
         'FEATURE_NEW_HEADER_FOOTER_ENABLED', False
     ),
