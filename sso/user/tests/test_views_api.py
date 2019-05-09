90f84fde7fe2d2fa18061e5f2a3688f44e85cc61
richtier <rikatee@gmail.com>
2019-05-09 10:35:29 +0100

    Prevent error caused by duplicate submission of user profile create

--- sso/user/tests/test_views_api.py
+++ sso/user/tests/test_views_api.py
@@ -4,8 +4,8 @@ from unittest import mock
 from django.urls import reverse
 from rest_framework.test import APIClient
 
-from sso.user.tests.factories import UserFactory
-from sso.user.models import User, UserProfile
+from sso.user.tests import factories
+from sso.user import models
 from sso.verification.models import VerificationCode
 
 
@@ -30,7 +30,7 @@ def test_create_user_api(api_client):
         'verification_code': mock.ANY
     }
 
-    assert User.objects.filter(email=new_email).count() == 1
+    assert models.User.objects.filter(email=new_email).count() == 1
 
 
 @pytest.mark.django_db
@@ -45,7 +45,7 @@ def test_create_user_api_invalid_password(api_client):
     )
     assert response.status_code == 400
 
-    assert User.objects.filter(email=new_email).count() == 0
+    assert models.User.objects.filter(email=new_email).count() == 0
 
 
 @pytest.mark.django_db
@@ -63,7 +63,7 @@ def test_create_user_api_exception_rollback(mock_create, api_client):
             format='json'
         )
         assert response.status_code == 500
-    assert User.objects.count() == 0
+    assert models.User.objects.count() == 0
     assert VerificationCode.objects.count() == 0
 
 
@@ -83,20 +83,21 @@ def test_create_user_api_verification_exception_rollback(
             format='json'
         )
         assert response.status_code == 500
-    assert User.objects.count() == 0
+    assert models.User.objects.count() == 0
     assert VerificationCode.objects.count() == 0
 
 
 @pytest.mark.django_db
 def test_create_user_profile(api_client):
-    user = UserFactory()
-    data = {'first_name': 'john',
-            'last_name': 'smith',
-            'mobile_phone_number': '0203044213',
-            'job_title': 'Director',
-            }
+    user = factories.UserFactory()
+    data = {
+        'first_name': 'john',
+        'last_name': 'smith',
+        'mobile_phone_number': '0203044213',
+        'job_title': 'Director',
+    }
 
-    assert UserProfile.objects.filter(user=user).count() == 0
+    assert models.UserProfile.objects.filter(user=user).count() == 0
     api_client.force_authenticate(user=user)
     response = api_client.post(
         reverse('api:user-create-profile'),
@@ -104,12 +105,32 @@ def test_create_user_profile(api_client):
         format='json'
     )
 
-    instance = UserProfile.objects.last()
+    instance = models.UserProfile.objects.last()
     assert response.status_code == 201
     assert instance.first_name == data['first_name']
     assert instance.last_name == data['last_name']
     assert instance.job_title == data['job_title']
     assert instance.mobile_phone_number == data['mobile_phone_number']
+
+
+@pytest.mark.django_db
+def test_create_user_profile_already_exists(api_client):
+    profile = factories.UserProfileFactory()
+    data = {
+        'first_name': 'john',
+        'last_name': 'smith',
+        'mobile_phone_number': '0203044213',
+        'job_title': 'Director',
+    }
+
+    api_client.force_authenticate(user=profile.user)
+    response = api_client.post(
+        reverse('api:user-create-profile'),
+        data,
+        format='json'
+    )
+
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
