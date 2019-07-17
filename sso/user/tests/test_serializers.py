923cea5f7e1d8b1cdec40c07ffd45f8643fca1b0
richtier <rikatee@gmail.com>
2019-07-17 10:18:25 +0100

    Expose has_user_profile on session user retrieval

--- sso/user/tests/test_serializers.py
+++ sso/user/tests/test_serializers.py
@@ -1,26 +1,20 @@
 import pytest
-from unittest.mock import Mock
 
-from sso.user.serializers import CreateUserProfileSerializer
-from sso.user.serializers import CreateUserSerializer
-from sso.user.tests.factories import UserFactory
-
-
-@pytest.fixture()
-def user():
-    return UserFactory()
+from sso.user import serializers
+from sso.user.tests import factories
 
 
 @pytest.mark.django_db
-def test_createuserprofileserializer_deserialization(user):
-    data = {'first_name': 'john',
-            'last_name': 'smith',
-            'mobile_phone_number': '0203044213',
-            'job_title': 'Director',
-            }
-    request = Mock()
-    request.user = user
-    serializer = CreateUserProfileSerializer(
+def test_createuserprofileserializer_deserialization(rf):
+    data = {
+        'first_name': 'john',
+        'last_name': 'smith',
+        'mobile_phone_number': '0203044213',
+        'job_title': 'Director',
+    }
+    request = rf.get('/')
+    request.user = factories.UserFactory.create()
+    serializer = serializers.CreateUserProfileSerializer(
         data=data, context={'request': request}
     )
     assert serializer.is_valid()
@@ -34,7 +28,7 @@ def test_createuserprofileserializer_deserialization(user):
 @pytest.mark.django_db
 def test_createuserserializer_deserialization():
     data = {'email': 'test@12345.com', 'password': 'Mypa11w0rd1'}
-    serializer = CreateUserSerializer(data=data)
+    serializer = serializers.CreateUserSerializer(data=data)
     assert serializer.is_valid()
     instance = serializer.save()
 
@@ -46,26 +40,42 @@ def test_createuserserializer_deserialization():
 @pytest.mark.django_db
 def test_createuserserializer_invalid_password_length():
     data = {'email': 'test@12345.com', 'password': 'AB123'}
-    serializer = CreateUserSerializer(data=data)
+    serializer = serializers.CreateUserSerializer(data=data)
     assert not serializer.is_valid()
 
 
 @pytest.mark.django_db
 def test_createuserserializer_invalid_password_numeric():
     data = {'email': 'test@12345.com', 'password': '1234567891'}
-    serializer = CreateUserSerializer(data=data)
+    serializer = serializers.CreateUserSerializer(data=data)
     assert not serializer.is_valid()
 
 
 @pytest.mark.django_db
 def test_createuserserializer_invalid_password_alpha():
     data = {'email': 'test@12345.com', 'password': 'ABCDEFGHIJK'}
-    serializer = CreateUserSerializer(data=data)
+    serializer = serializers.CreateUserSerializer(data=data)
     assert not serializer.is_valid()
 
 
 @pytest.mark.django_db
 def test_createuserserializer_invalid_password_password():
     data = {'email': 'test@12345.com', 'password': '1passWord2'}
-    serializer = CreateUserSerializer(data=data)
+    serializer = serializers.CreateUserSerializer(data=data)
     assert not serializer.is_valid()
+
+
+@pytest.mark.django_db
+def test_user_serializer_no_profile():
+    user = factories.UserFactory.create()
+    serializer = serializers.UserSerializer(user)
+
+    assert serializer.data['has_user_profile'] is False
+
+
+@pytest.mark.django_db
+def test_user_serializer_has_profile():
+    user_profile = factories.UserProfileFactory.create()
+    serializer = serializers.UserSerializer(user_profile.user)
+
+    assert serializer.data['has_user_profile'] is True
