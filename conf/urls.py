99a1a818f243ed50c3c1da3a5219afb2e976f04c
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-11-11 11:02:40 +0000

    urls updated

--- conf/urls.py
+++ conf/urls.py
@@ -214,13 +214,14 @@ urlpatterns = [
     ),
 ]
 
-authbroker_urls = [
-    url(
-        r'^admin/login/$',
-        RedirectView.as_view(url=reverse_lazy('authbroker_client:login'), query_string=True, )
-    ),
-    url('^auth/', include('authbroker_client.urls')),
-]
 
 if settings.FEATURE_ENFORCE_STAFF_SSO_ENABLED:
+    authbroker_urls = [
+        url(
+            r'^admin/login/$',
+            RedirectView.as_view(url=reverse_lazy('authbroker_client:login'),
+                                 query_string=True, )
+        ),
+        url('^auth/', include('authbroker_client.urls')),
+    ]
     urlpatterns = [url('^', include(authbroker_urls))] + urlpatterns
