ed228ce8a5dc7459068b0a7e13926ffe10da218d
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-11-12 09:42:47 +0000

    remove user test

--- core/middleware.py
+++ core/middleware.py
@@ -32,14 +32,10 @@ class AdminPermissionCheckMiddleware(MiddlewareMixin):
             return True
         return False
 
-    def is_authenticated_user(self, user):
-        if user:
-            return user.is_authenticated()
-        return False
 
     def process_view(self, request, view_func, view_args, view_kwarg):
         # Django admin users without permission will be displayed custom message to request access
-        if self.is_authenticated_user(request.user):
+        if request.user.is_authenticated():
             if self.is_admin_name_space(request) or request.path_info.startswith('/admin/login'):
                 if not request.user.is_staff:
                     return HttpResponse(self.SSO_UNAUTHORISED_ACCESS_MESSAGE, status=401)
