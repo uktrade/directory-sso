a7606cb001af8d7d7684be925f6ac2561a7dfd95
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-13 16:00:57 +0000

    add option to create test users programmatically

--- sso/testapi/views.py
+++ sso/testapi/views.py
@@ -1,6 +1,10 @@
+from uuid import uuid4
+
+import factory
 from django.conf import settings
 from django.http import HttpResponseNotFound
 from django.shortcuts import get_list_or_404
+from django.db.utils import IntegrityError
 from rest_framework.generics import (
     DestroyAPIView,
     RetrieveAPIView,
@@ -12,6 +16,7 @@ from rest_framework.response import Response
 import core.mixins
 from conf.signature import SignatureCheckPermission
 from sso.user import models
+from sso.user.tests.factories import UserProfileFactory
 
 
 class UserByEmailAPIView(
@@ -62,7 +67,7 @@ class TestUsersAPIView(core.mixins.NoIndexMixin, DestroyAPIView):
     permission_classes = [SignatureCheckPermission]
     authentication_classes = []
     queryset = models.User.objects.all()
-    http_method_names = 'delete'
+    http_method_names = ['delete', 'post']
 
     def dispatch(self, *args, **kwargs):
         if not settings.FEATURE_FLAGS['TEST_API_ON']:
@@ -77,3 +82,35 @@ class TestUsersAPIView(core.mixins.NoIndexMixin, DestroyAPIView):
         for user in test_users:
             user.delete()
         return Response(status=204)
+
+    def post(self, request, **kwargs):
+        try:
+            profile = UserProfileFactory.create(
+                user__email=request.data.get(
+                    'email', f'test+{uuid4()}@directory.uktrade.digital'),
+                first_name=request.data.get(
+                    'first_name', factory.Faker('first_name')),
+                last_name=request.data.get(
+                    'last_name', factory.Faker('last_name')),
+                job_title=request.data.get('job_title', 'AUTOMATED TESTS'),
+                mobile_phone_number=request.data.get(
+                    'mobile_phone_number', factory.Faker('msisdn')),
+            )
+            profile.save()
+            user = profile.user
+            user.set_password(request.data.get('password', 'password'))
+            user.save()
+            # don't use default UserSerializer as we want flat structure in response
+            data = {
+                'email': user.email,
+                'password': request.data.get('password', 'password'),
+                'id': user.id,
+                'first_name': profile.first_name,
+                'last_name': profile.last_name,
+                'job_title': profile.job_title,
+                'mobile_phone_number': profile.mobile_phone_number,
+            }
+            return Response(data=data)
+        except IntegrityError as ex:
+            data = {'error': str(ex)}
+            return Response(status=400, data=data)
