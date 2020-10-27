f2f20c199877aa2a8f019d613c55cd94c44159cd
Bob Meredith <bob.meredith@ntlworld.com>
2020-10-27 16:49:35 +0000

    Revert user-views

--- sso/user/views.py
+++ sso/user/views.py
@@ -56,10 +56,6 @@ class LoginView(RedirectToNextMixin, allauth_views.LoginView):
 
 
 class LogoutView(RedirectToNextMixin, allauth_views.LogoutView):
-
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
     pass
 
 
