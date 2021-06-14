359f085e414bf3fce9ae7a54735fd16b16af6d48
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-06-14 16:32:51 +0100

    Archieve user will not delete submission record from directoy_forms_api

--- sso/management/commands/archive_users.py
+++ sso/management/commands/archive_users.py
@@ -7,14 +7,13 @@ from django.core.management.commands.migrate import Command as MigrateCommand
 from django.db.models import Q
 
 from sso.user.models import DataRetentionStatistics, User
-
+from directory_forms_api_client.client import forms_api_client
 
 class Command(MigrateCommand):
     """
     Archive users who's not logged in past three years
     or created three years ago and never logged in
     """
-
     def handle(self, *args, **options):
         queryset = User.objects.all()
         total_users = queryset.count()
@@ -32,6 +31,7 @@ class Command(MigrateCommand):
         total_old_users = old_users.count()
         # Delete related user data in directory-api
         for user in old_users:
+            # directory-api data removal
             response = api_client.company.delete_company_by_sso_id(
                 sso_id=user.id, request_key=settings.DIRECTORY_API_SECRET
             )
@@ -43,10 +43,21 @@ class Command(MigrateCommand):
                     )
                 )
                 raise Exception('Something went wrong in directory-api')
-            else:
-                json_response = json.loads(response.content)
-                company += int(json_response.get('company_counter', 0))
-                company_user += int(json_response.get('company_user_count', 0))
+
+            # form-api data removal
+            forms_api_response = forms_api_client.delete_submissions(email_address=user.email)
+            if forms_api_response.status_code != 204:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'Something went wrong while deleting {user}'
+                        f'(user_id:{user.id}) \'s data in directory-forms-api repo'
+                    )
+                )
+                raise Exception('Something went wrong in directory-forms-api')
+
+            json_response = json.loads(response.content)
+            company += int(json_response.get('company_counter', 0))
+            company_user += int(json_response.get('company_user_count', 0))
 
         # Delete sso users
         old_users.delete()
