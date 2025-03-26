413d28ca827098043a3a15da87054b36a3221fd4
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-03-26 16:12:14 +0000

    added test for new Command

--- sso/tests/test_tasks.py
+++ sso/tests/test_tasks.py
@@ -1,8 +1,10 @@
 from unittest import mock
 
+import pytest
 from django.test.utils import override_settings
 
 from sso import tasks
+from sso.user.tests.factories import UserFactory, UserProfileFactory
 
 
 @override_settings(
@@ -21,3 +23,14 @@ def test_notify_users_task(mocked_call_command):
 def test_archive_users_task(mocked_call_command):
     tasks.archive_users()
     mocked_call_command.assert_called_once()
+
+
+@mock.patch('sso.management.commands.obsfucate_personal_details.Command.mask_user_profile')
+@mock.patch('sso.management.commands.obsfucate_personal_details.Command.mask_user')
+@pytest.mark.django_db
+def test_obsfucate_personal_details(mock_mask_user, mock_mask_user_profile):
+    UserFactory()
+    UserProfileFactory()
+    tasks.obsfucate_personal_details()
+    assert mock_mask_user.call_count == 1
+    assert mock_mask_user_profile.call_count == 1
