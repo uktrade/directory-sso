6dff620c093db309f0c1dcf5b862ebfb94eb87be
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-03-27 10:21:26 +0000

    Add guard against obsfucate in prod

--- sso/management/commands/obsfucate_personal_details.py
+++ sso/management/commands/obsfucate_personal_details.py
@@ -1,5 +1,6 @@
 import argparse
 
+from django.conf import settings
 from django.core.management import BaseCommand
 
 from sso.user.models import User, UserProfile
@@ -63,6 +64,10 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):  # noqa: C901
+
+        if settings.APP_ENVIRONMENT.lower() == 'production':
+            self.stdout.write(self.style.WARNING('Running in Production environment is disabled - exiting'))
+            return
 
         # Obsfucate User Data
         for user in User.objects.all():
--- sso/tests/test_tasks.py
+++ sso/tests/test_tasks.py
@@ -1,36 +1,83 @@
-from unittest import mock
+import argparse
 
-import pytest
-from django.test.utils import override_settings
+from django.conf import settings
+from django.core.management import BaseCommand
 
-from sso import tasks
-from sso.user.tests.factories import UserFactory, UserProfileFactory
-
-
-@override_settings(
-    FEATURE_REDIS_USE_SSL=True,
-)
-@mock.patch('sso.tasks.call_command')
-def test_notify_users_task(mocked_call_command):
-    tasks.notify_users()
-    mocked_call_command.assert_called_once()
+from sso.user.models import User, UserProfile
 
 
-@override_settings(
-    FEATURE_REDIS_USE_SSL=True,
-)
-@mock.patch('sso.tasks.call_command')
-def test_archive_users_task(mocked_call_command):
-    tasks.archive_users()
-    mocked_call_command.assert_called_once()
+class Command(BaseCommand):
+    help = 'Obsfucate Personal Details'
 
+    START_INDEX = 1
+    END_INDEX = -1
+    MASK_CHAR = '*'
 
-@mock.patch('sso.management.commands.obsfucate_personal_details.Command.mask_user_profile')
-@mock.patch('sso.management.commands.obsfucate_personal_details.Command.mask_user')
-@pytest.mark.django_db
-def test_obsfucate_personal_details(mock_mask_user, mock_mask_user_profile):
-    UserFactory()
-    UserProfileFactory()
-    tasks.obsfucate_personal_details()
-    assert mock_mask_user.call_count == 2
-    assert mock_mask_user_profile.call_count == 1
+    def mask_email_data(self, data):
+        if not data:
+            return data
+        name = data.split('@')[0]
+        address = data.split('@')[1]
+        name = self.mask_string_data(name)
+        address = self.mask_string_data(address)
+        return f'{name}@{address}'
+
+    def mask_string_data(self, data):
+        if not data:
+            return data
+        ret = f'{data[:self.START_INDEX]}{self.MASK_CHAR * len(data[self.START_INDEX:self.END_INDEX])}{data[self.END_INDEX:]}'  # noqa:E501
+        return ret
+
+    def mask_json_data(self, data, fields):
+        for field in fields:
+            try:
+                masked_field = self.mask_string_data(data[field])
+                data[field] = masked_field
+            except KeyError:
+                pass
+            else:
+                data[field] = masked_field
+        return data
+
+    def mask_user(self, user, options):
+        user.email = self.mask_email_data(user.email)
+        user.first_name = self.mask_string_data(user.first_name)
+        user.last_name = self.mask_string_data(user.last_name)
+
+        if options['dry_run'] is False:
+            user.save()
+
+    def mask_user_profile(self, user_profile, options):
+        user_profile.first_name = self.mask_string_data(user_profile.first_name)
+        user_profile.last_name = self.mask_string_data(user_profile.last_name)
+        user_profile.mobile_phone_number = self.mask_string_data(user_profile.mobile_phone_number)
+
+        if options['dry_run'] is False:
+            user_profile.save()
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--dry_run',
+            action=argparse.BooleanOptionalAction,
+            default=False,
+            help='Show summary output only, do not update data',
+        )
+
+    def handle(self, *args, **options):  # noqa: C901
+
+        if settings.APP_ENVIRONMENT.lower() == 'production':
+            self.stdout.write(self.style.WARNING('Running in Production environment is disabled - exiting'))
+            return
+
+        # Obsfucate User Data
+        for user in User.objects.all():
+            self.mask_user(user, options)
+
+        # Obsfucate UserProfile  Data
+        for user_profile in UserProfile.objects.all():
+            self.mask_user_profile(user_profile, options)
+
+        if options['dry_run'] is True:
+            self.stdout.write(self.style.WARNING('Dry run -- no data updated.'))
+
+        self.stdout.write(self.style.SUCCESS('All done, bye!'))
